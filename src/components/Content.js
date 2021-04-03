cd884094629b13b35dab7827566c7f4c0da33fe4
Pea <pea@misfist.com>
2021-04-02 23:44:46 -0400

    Display form on Contribute page only

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,46 +4,50 @@ import slugify from 'slugify'
 import classNames from 'classnames'
 import ContributeForm from './ContributeForm'
 
-const Content = ( { post } ) => {
-  const nodeType = post?.contentType?.node?.nodeType || '';
-  const slug = slugify( nodeType, { lower: true } );
-  const classes = classNames(
-    slug,
-    'section-inner'
-  );
+const Content = ({ post }) => {
+  const nodeType = post?.contentType?.node?.nodeType || ''
+  const slug = slugify(nodeType, { lower: true })
+  const classes = classNames(slug, 'section-inner')
 
   return (
-    <article
-        className={classes}
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header className="entry-header">
-          <h1 className="entry-title" itemProp="headline">{post.title}</h1>
+    <article className={classes} itemScope itemType="http://schema.org/Article">
+      <header className="entry-header">
+        <h1 className="entry-title" itemProp="headline">
+          {post.title}
+        </h1>
 
-          {/* if we have a featured image for this post let's display it .featuredImage.node.localFile.childImageSharp.gatsbyImageData*/}
-          {post?.featuredImage?.node?.localFile?.childImageSharp?.gatsbyImageData && (
-            <figure className="featured-image">
-              <GatsbyImage
-                image={post.featuredImage.node.localFile.childImageSharp.gatsbyImageData}
-                alt={post.featuredImage.node.altText}
+        {/* if we have a featured image for this post let's display it .featuredImage.node.localFile.childImageSharp.gatsbyImageData*/}
+        {post?.featuredImage?.node?.localFile?.childImageSharp
+          ?.gatsbyImageData && (
+          <figure className="featured-image">
+            <GatsbyImage
+              image={
+                post.featuredImage.node.localFile.childImageSharp.gatsbyImageData
+              }
+              alt={post.featuredImage.node.altText}
+            />
+            {post?.featuredImage?.node?.caption && (
+              <figcaption
+                className="caption"
+                dangerouslySetInnerHTML={{
+                  __html: post.featuredImage.node.caption,
+                }}
               />
-              {post?.featuredImage?.node?.caption && (
-                <figcaption className="caption" dangerouslySetInnerHTML={{ __html: post.featuredImage.node.caption }} />
-              )}
-            </figure>
-          )}
-        </header>
+            )}
+          </figure>
+        )}
+      </header>
 
-        { post.slug === 'contribute' ? (
-          <ContributeForm />
-        ) : (
-          !!post.content && (
-            <div className="entry-content" itemProp="articleBody" dangerouslySetInnerHTML={{ __html: post.content }} />
-          )      
-        ) }
+      {!!post.content && (
+        <div
+          className="entry-content"
+          itemProp="articleBody"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      )}
 
-      </article>
+      {post.slug === 'contribute' && <ContributeForm />}
+    </article>
   )
 }
 
