fab0be4bc2d1c9a8622149ee83f9ae5fa7d8c5ef
Pea <pea@misfist.com>
2021-04-02 22:48:54 -0400

    Add Alert component

---
+++ src/components/Alert.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from 'react'
+import classNames from 'classnames'
+import parse from "html-react-parser"
+
+const Alert = ( { props: { message, type, dismissible } } ) => {
+  const [alertOpenState, setAlertOpenState] = useState(true)
+
+  const handleClose = ( event ) => {
+    setAlertOpenState(false)
+    event.target.classList.add( 'closed' );
+    console.log( message, type, dismissible )
+  }
+
+  const classes = classNames(
+    'alert',
+    `alert-${type}`,
+    {
+      'dismissible': dismissible
+    }
+  );
+
+  return (
+    <div 
+      className={classes}
+      aria-expanded={alertOpenState}
+      role="alertdialog"
+    >
+      { dismissible && (
+        <div 
+         className={close}
+         aria-label="Close"
+         onClick={handleClose}
+        ><span className="screen-reader-text">Close Alert</span></div>
+      ) }
+      {message}
+    </div>
+  )
+}
+
+export default Alert
--- src/components/ContributeForm.js
+++ src/components/ContributeForm.js
@@ -1,23 +1,37 @@
 import React, { useState, useRef } from 'react'
+import Alert from './Alert'
 
 const ContributeForm = () => {
-  const [formIsSubmitted, setformIsSubmitted] = useState(false)
-  const [formState, setFormState] = useState(null)
+  const [formIsSubmitted, setformIsSubmitted] = useState( false )
+  const [formState, setFormState] = useState( null )
+  const [formStatus, setFormStatus] = useState( null )
+  const [responseProps, setResponseProps] = useState( {} )
   const message = useRef( null );
-  const form = useRef( null )
 
-  const successMessage = `<span className="alert alert-success">Thanks so much for contributing!!</span>`
+  const successMessage = 'Thanks so much for contributing!!'
 
-  const errorMessage = `<span className="alert alert-error">Yikes. Something went wrong. Could you please try again?</span>`
+  const errorMessage = 'Yikes. Something went wrong. Could you please try again?'
 
   const handleSuccess = ( event, response ) => {
     setformIsSubmitted( true );
-    console.log( event, response );
+    setFormState( 'success' );
+    setResponseProps( {
+      message: successMessage,
+      type: 'success',
+      dismissible: true
+    } )
+    console.log( response, responseProps );
   }
 
   const handleError = ( event, error ) => {
-    setformIsSubmitted( false );
-    console.log( event, error );
+    setformIsSubmitted( true );
+    setFormState( 'error' );
+    setResponseProps( {
+      message: errorMessage,
+      type: 'error',
+      dismissible: false
+    } )
+    console.error( error, responseProps );
   }
 
   const handleChange = ( event ) => {
@@ -28,50 +42,51 @@ const ContributeForm = () => {
   const handleSubmit = ( event ) => {
     event.preventDefault()
     
-    // let form = event.target;
     let formData = new FormData( event.target )
     fetch('/', {
       method: 'POST',
       headers: { "Content-Type": "multipart/form-data" },
       body: new URLSearchParams(formData).toString()
-    //   body: encode({
-    //     'form-name': form.getAttribute('name'),
-    //     ...formState,
-    //   }),
     })
     .then( ( response ) => {
-        navigate( form.getAttribute('action') )
+      if( response.status == 200 ) {
         handleSuccess( event, response )
+      } else {
+        handleError( event, response )
+      }
     })
     .catch( ( error ) => { 
-        console.log( error )
-        handleError( event, error )
+      handleError( event, error )
     })
   }
 
   return (
-    <form 
-        name="contribute" 
-        method="POST"
-        action="/contribute"
-        data-netlify="true" 
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-    >
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" onChange={handleChange}/>
-      <label htmlFor="email">Email</label>
-      <input type="email" name="email" onChange={handleChange} />
-      <label htmlFor="subject">Subject</label>
-      <input type="text" name="subject" onChange={handleChange} />
-      <label htmlFor="comments">Comments</label>
-      <textarea name="comments" onChange={handleChange} />
-      <label htmlFor="picture">Picture</label>
-      <input type="file" name="picture" />
-      <button type="submit">Send</button>
-      <div className="form-message" ref={message}></div>
-      <input type="hidden" name="form-name" value="contribute" />
-    </form>
+    <div className="contribute-form">
+      {formIsSubmitted && (
+        <Alert props={responseProps} />
+      )}
+      <form 
+          name="contribute" 
+          method="POST"
+          action="/contribute"
+          data-netlify="true" 
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" onChange={handleChange}/>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" aria-required="true" onChange={handleChange} />
+        <label htmlFor="subject">Subject</label>
+        <input type="text" name="subject" onChange={handleChange} />
+        <label htmlFor="comments">Comments</label>
+        <textarea name="comments" onChange={handleChange} />
+        <label htmlFor="picture">Picture</label>
+        <input type="file" aria-required="true" name="picture" />
+        <button type="submit">Send</button>
+        <input type="hidden" name="form-name" value="contribute" />
+      </form>
+    </div>
   )
 }
 
